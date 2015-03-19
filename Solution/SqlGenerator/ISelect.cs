b76fc466d9b294a50d0df200da5d957c20a64287
Szymon Sasin <szymon@sasin.eu>
2015-03-19 22:10:12 +0000

    Added new progressive interfaces

--- Solution/SqlGenerator/ISelect.cs
+++ Solution/SqlGenerator/ISelect.cs
@@ -1,13 +1,52 @@
 ﻿namespace SqlGenerator
 {
+    //Select
+    //    .AllValues  | .Parameter
+    //From <tableName>
+    //Where a =b
+    //    AND c = d
+
     public interface ISelect
     {
-        ISelect From(string tableName, string alias = null);
+        IFrom AllColumns();
 
-        ISelect Alias(string alias);
+        IFrom Columns(params string[] properties);
+    }
 
-        ISelect Where(string whereClase);
+    public interface IFrom
+    {
+        IFrom Table(string table);
 
+        IFrom Table(string table, string alias);
+    }
+
+    public interface IWhere : IGenerate
+    {
+        IWhereOperator For(string leftValue);
+    }
+
+    public interface IWhereOperator
+    {
+        IWhereJoin IsEqualTo(string rightValue);
+
+        IWhereJoin IsGreat(string rightValue);
+
+        IWhereJoin IsLower(string rightValue);
+
+        IWhereJoin InBetween(string rightValue);
+
+        IWhereJoin Like(string rightValue);
+    }
+
+    public interface IWhereJoin : IGenerate
+    {
+        IWhere And();
+
+        IWhere Or();
+    }
+
+    public interface IGenerate
+    {
         string ToString();
     }
 }
